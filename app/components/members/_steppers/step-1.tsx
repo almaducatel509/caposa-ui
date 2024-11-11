164a9c91eb17edd896f8616f3323fccf09b9a9bd
Alma ducatel <89481444+almaducatel509@users.noreply.github.com>
2024-11-11 01:00:31 -0500

    Update step-1.tsx
    new changement in member

--- app/components/members/_steppers/step-1.tsx
+++ app/components/members/_steppers/step-1.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Step1Data, ErrorMessages } from '../validations';
 import TitleDetails from './title-details';
-import { Input, RadioGroup, Radio, DatePicker, Autocomplete, AutocompleteItem } from '@nextui-org/react'
-import { useDateFormatter } from "@react-aria/i18n";
+import { Input, RadioGroup, Radio, DateInput, Autocomplete, AutocompleteItem } from '@nextui-org/react'
 import UploadImage from '../../core/upload-file';
-import { parseDate, getLocalTimeZone } from "@internationalized/date";
+import { parseDate, getLocalTimeZone, DateValue } from "@internationalized/date";
+import { PiEyeSlash } from "react-icons/pi";
+import { PiEye } from "react-icons/pi";
 
 interface Step1Props {
     formData: Step1Data;
@@ -17,9 +18,7 @@ type Option = {
 }
 
 const Step1: React.FC<Step1Props> = ({ formData, setFormData, errors }) => {
-    const color = 'success'
-    const [value, setValue] = React.useState<any>();
-    const [selected, setSelected] = React.useState<any>("");
+    const color = 'success';
     const options: Option[] = [
         { label: 'Artibonite', value: 'Artibonite' },
         { label: 'Centre', value: 'Centre' },
@@ -31,91 +30,101 @@ const Step1: React.FC<Step1Props> = ({ formData, setFormData, errors }) => {
         { label: 'Ouest', value: 'Ouest' },
         { label: 'Sud', value: 'Sud' },
         { label: 'Sud-Est', value: 'Sud-Est' },
-    ]
+    ];
+
     const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         const { name, value } = e.target;
-        setFormData({ [name]: value });
+        setFormData({ ...formData, [name]: value });
     };
 
-    const handleRadio = (value: any) => {
-        setFormData({ ...formData, gender: value })
+    const handleRadio = (value: string) => {
+        setFormData({ gender: value });
+        console.log("Genre sélectionné :", value); // Console pour vérifier le sexe sélectionné
+    };
 
-    }
-    const handleChangeDate = (value: any) => {
+    const handleChangeDate = (date: DateValue) => {
+        const formattedDate = date.toString(); // Formate la date en YYYY-MM-DD
+        setFormData({ date_of_birthday: formattedDate });
+        console.log('Date de naissance mise à jour:', formattedDate); // Console pour vérifier la date sélectionnée
+    };
 
-        setFormData({ ...formData, date_of_birthday: `${value.year}-${value.month < 10 ? '0' : ""}${value.month}-${value.day < 10 ? '0' : ''}${value.day}` })
-    }
-    let d: any = new Date().toLocaleDateString("fr-FR").split("/")
-    d = `${d[2]}-${d[1]}-${d[0]}`;
     const handleChangeDepartment = (value:any)=>{
-        console.log(value)
-        setFormData({ ...formData, department: value })
+            console.log(value)
+            setFormData({ ...formData, department: value })
     }
+    const [isVisible, setIsVisible] = React.useState(false);
+    const [isConfirmVisible, setIsConfirmVisible] = React.useState(false);
+
+    const toggleVisibility = () => setIsVisible(!isVisible);
+    const toggleConfirmVisibility = () => setIsConfirmVisible(!isConfirmVisible);
     return (
         <div>
-            <TitleDetails text1={'Informations personnelles'} text2={'Fournir vos informations personnelles'} />
+            <TitleDetails text1="Informations personnelles" text2="Fournir vos informations personnelles" />
             <div className="grid grid-cols-1 gap-8 md:grid-cols-3">
                 <div className="space-y-2">
-                    <Input type={'text'} name='first_name' value={formData.first_name} label={'Prénom'} onChange={handleChange} isRequired />
+                    <Input type="text" name="first_name" value={formData.first_name} label="Prénom" onChange={handleChange} isRequired />
                     {errors.first_name && <div className='text-destructive text-red-600'>{errors.first_name}</div>}
                 </div>
                 <div className="space-y-2">
-                    <Input type={'text'} name='last_name' value={formData.last_name} label={'Nom'} onChange={handleChange} isRequired />
+                    <Input type="text" name="last_name" value={formData.last_name} label="Nom" onChange={handleChange} isRequired />
                     {errors.last_name && <div className='text-destructive text-red-600'>{errors.last_name}</div>}
                 </div>
                 <div className="space-y-2">
+                    <div className="flex flex-col gap-3">
                     <RadioGroup
                         label="Choisir le sexe"
                         isRequired
                         orientation="horizontal"
-                        name='gender'
+                        name="gender"
                         value={formData.gender}
                         onValueChange={handleRadio}
+                        color="primary"
+                        defaultValue="F"
+
                     >
                         <Radio value="M">M</Radio>
                         <Radio value="F">F</Radio>
                     </RadioGroup>
+
+                    </div>
                     {errors.gender && <div className='text-destructive text-red-600'>{errors.gender}</div>}
                 </div>
                 <div className="space-y-2">
-                    <div className="flex w-full flex-wrap md:flex-nowrap gap-4">
-                        <DatePicker
-                            label="Date de naissance"
-                            className=""
-                            isRequired
-                            value={parseDate(formData.date_of_birthday || d)}
-                            onChange={handleChangeDate}
-                        />
-                    </div>
-
-                    {errors.date_of_birthday && <div className='text-destructive text-red-600'>{errors.date_of_birthday}</div>}
+                    <DateInput
+                        label="Date de naissance"
+                        description="Veuillez entrer la date au format AAAA-MM-JJ"
+                        isInvalid={false} // Par défaut, non invalide
+                        value={parseDate(formData.date_of_birthday || "1997-06-09")}
+                        onChange={handleChangeDate}
+                        isRequired
+                        errorMessage={errors.date_of_birthday}
+                        className="max-w-xs text-black"
+                    />
                 </div>
                 <div className="space-y-2">
-                    <Input type={'text'} name='id_number' value={formData.id_number} label={'Numero identité'} onChange={handleChange} isRequired />
+                    <Input type="text" name="id_number" value={formData.id_number} label="Numero identité" onChange={handleChange} isRequired />
                     {errors.id_number && <div className='text-destructive text-red-600'>{errors.id_number}</div>}
                 </div>
                 <div className="space-y-2">
-                    <Input type={'text'} name='phone_number' value={formData.phone_number} label={'Téléphone'} onChange={handleChange} isRequired />
+                    <Input type="text" name="phone_number" value={formData.phone_number} label="Téléphone" onChange={handleChange} isRequired />
                     {errors.phone_number && <div className='text-destructive text-red-600'>{errors.phone_number}</div>}
                 </div>
                 <div className="space-y-2">
-                    <Input type={'email'} name='email' value={formData.email} label={'Email'} onChange={handleChange} />
-                    {errors.email && <div className='text-destructive text-red-600'>{errors.email}</div>}
+                    <Input type="email" name="email" value={formData.email} label="Email" onChange={handleChange} />
+                    {errors.email && <div className='text-red-600'>{errors.email}</div>}
                 </div>
                 <div className="space-y-2">
-                    <Input type={'text'} name='address' value={formData.address} label={'Adresse'} onChange={handleChange} isRequired />
-                    {errors.address && <div className='text-destructive text-red-600'>{errors.address}</div>}
+                    <Input type="text" name="address" value={formData.address} label="Adresse" onChange={handleChange} isRequired />
+                    {errors.address && <div className='text-red-600'>{errors.address}</div>}
                 </div>
                 <div className="space-y-2">
                     <Autocomplete
                         label="Département"
-                        // variant="bordered"
                         defaultItems={options}
                         placeholder="Choisir un département"
                         className=""
                         selectedKey={formData.department}
                         isRequired
-                        // name="department"
                         onSelectionChange={handleChangeDepartment}
                     >
                         {(item) => <AutocompleteItem key={item.value}>{item.label}</AutocompleteItem>}
@@ -123,28 +132,80 @@ const Step1: React.FC<Step1Props> = ({ formData, setFormData, errors }) => {
                     {errors.department && <div className='text-destructive text-red-600'>{errors.department}</div>}
                 </div>
                 <div className="space-y-2 md:mt-6">
-                    <Input type={'text'} name='city' value={formData.city} label={'Ville'} onChange={handleChange} isRequired />
+                    <Input
+                        type="text"
+                        name="city"
+                        value={formData.city}
+                        label="Ville"
+                        onChange={handleChange}
+                        isRequired
+                    />
                     {errors.city && <div className='text-destructive text-red-600'>{errors.city}</div>}
                 </div>
-                <div className='space-y-2'>
-                    <UploadImage name='photo_url' data={null} fallback={""} description="Photo" />
+                <div className="space-y-2">
+                    <Input
+                        label="Password"
+                        variant="bordered"
+                        placeholder="Enter your password"
+                        endContent={
+                            <button className="focus:outline-none" type="button" onClick={toggleVisibility} aria-label="toggle password visibility">
+                                {isVisible ? (
+                                    <PiEyeSlash className="text-2xl text-default-400 pointer-events-none" />
+                                ) : (
+                                    <PiEye className="text-2xl text-default-400 pointer-events-none" />
+                                )}
+                            </button>
+                        }
+                        type={isVisible ? "text" : "password"}
+                        className="max-w-xs"
+                        name="password"
+                        value={formData.password}
+                        onChange={handleChange}
+                        isRequired
+                    />
+                    {errors.password && <div className="text-red-600">{errors.password}</div>}
+                </div>
 
+                <div className="space-y-2">
+                    <Input
+                        label="Confirm Password"
+                        variant="bordered"
+                        placeholder="Re-enter your password"
+                        endContent={
+                            <button className="focus:outline-none" type="button" onClick={toggleConfirmVisibility} aria-label="toggle password visibility">
+                                {isConfirmVisible ? (
+                                    <PiEyeSlash className="text-2xl text-default-400 pointer-events-none" />
+                                ) : (
+                                    <PiEye className="text-2xl text-default-400 pointer-events-none" />
+                                )}
+                            </button>
+                        }
+                        type={isConfirmVisible ? "text" : "password"}
+                        className="max-w-xs"
+                        name="confirm_password"
+                        value={formData.confirm_password}
+                        onChange={handleChange}
+                        isRequired
+                    />
+                    {errors.confirm_password && <div className="text-red-600">{errors.confirm_password}</div>}
+                </div>
+
+                <div className="space-y-2">
+                    <UploadImage
+                        name='photo_url'
+                        data={null}
+                        fallback={""}
+                        description="Photo"
+                        formData={formData}
+                        setFormData={setFormData}
+
+                    />
                     {errors.photo_url && <div className='text-destructive text-red-600'>{errors.photo_url}</div>}
                 </div>
             </div>
-
-
-            {/* <div>
-                <label>Last Name</label>
-                <input
-                    name="lastName"
-                    value={formData.lastName}
-                    onChange={handleChange}
-                />
-                {errors.lastName && <p>{errors.lastName}</p>}
-            </div> */}
         </div>
     );
 };
 
 export default Step1;
+
